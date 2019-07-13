54804a77e2e3c9be3cd50499e0725ac982dd0a3c
r4xjs <r4xjs@lowsec.net>
2019-07-13 09:43:32 +0200

    improve touchpad

---
+++ i3/disable_touchpad.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+xinput set-prop 14 --type=float 145 0 0 0 0 0 0 0 0 1
+xinput set-prop 14 313 0,0,0,0,0,0,0
---
+++ i3/enable_touchpad.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+xinput set-prop 14 --type=float 145 1 0 0 0 1 0 0 0 1
+xinput set-prop 14 313 2,3,0,0,1,3,0

3b06df514b49477d5a25167c3b569da84ca65af5
r4xjs <r4xjs@lowsec.net>
2019-11-02 11:40:07 +0100

    t490 arch settings

--- install.sh
+++ install.sh
@@ -8,34 +8,43 @@ if [ "${1}" == "full" ]; then
     mkdir ~/bin
     ln -s $(pwd)/scripts/fixes/wakeup_fix.sh ~/bin/wakeup_fix.sh
     ln -s $(pwd)/scripts/fixes/brightness.sh ~/bin/brightness.sh
-   
-    # bluetooth pulseaudio default setup
-    read -p "Do you want to setup bluetooth headphone to pulse/default.pa?" yn
-    if [ "$yn"  = "y" ]; then
-        echo "set-default-sink bluez_sink.04_5D_4B_DE_25_67.headset_head_unit" >> /etc/pulse/default.pa
-        echo "set bluez_sink.04_5D_4B_DE_25_67.headset_head_unit as default"
-    fi
 fi
 
-# tmux
-ln -s "$(pwd)"/tmux.conf ~/.tmux.conf
+if [ "${1}" == "remove" ]; then
+    rm ~/.i3
+    rm ~/bin/wakeup_fix.sh
+    rm ~/bin/brightness.sh
+    rm ~/.tmux.conf
+    rm ~/.zshrc
+    rm ~/.zsh/cache
+    rm ~/.config/htop/htoprc
+    rm ~/.local/share/nvim/site/autoload/plug.vim
+    rm ~/.config/nvim/init.vim
+    rm ~/.nvimrc
+    rm ~/.vimrc
+    rm ~/.vim/autoload/plug.vim
+else
+    # tmux
+    ln -s "$(pwd)"/tmux.conf ~/.tmux.conf
+    
+    # zsh
+    ln -s "$(pwd)"/zshrc ~/.zshrc
+    mkdir -p ~/.zsh/cache
+    
+    # htop
+    mkdir ~/.config/htop
+    ln -s $(pwd)/htoprc ~/.config/htop/htoprc
+    
+    # nvim
+    mkdir -p ~/.config/nvim
+    mkdir -p ~/.local/share/nvim/site/autoload/
+    ln -s $(pwd)/nvim/plug.vim ~/.local/share/nvim/site/autoload/plug.vim
+    ln -s $(pwd)/nvim/init.vim ~/.config/nvim/init.vim
+    ln -s $(pwd)/nvim/init.vim ~/.nvimrc
+    
+    # vim
+    mkdir -p ~/.vim/autoload
+    ln -s $(pwd)/nvim/init.vim ~/.vimrc
+    ln -s $(pwd)/nvim/plug.vim ~/.vim/autoload/plug.vim
+fi
 
-# zsh
-ln -s "$(pwd)"/zshrc ~/.zshrc
-mkdir -p ~/.zsh/cache
-
-# htop
-mkdir ~/.config/htop
-ln -s $(pwd)/htoprc ~/.config/htop/htoprc
-
-# nvim
-mkdir -p ~/.config/nvim
-mkdir -p ~/.local/share/nvim/site/autoload/
-ln -s $(pwd)/nvim/plug.vim ~/.local/share/nvim/site/autoload/plug.vim
-ln -s $(pwd)/nvim/init.vim ~/.config/nvim/init.vim
-ln -s $(pwd)/nvim/init.vim ~/.nvimrc
-
-# vim
-mkdir -p ~/.vim/autoload
-ln -s $(pwd)/nvim/init.vim ~/.vimrc
-ln -s $(pwd)/nvim/plug.vim ~/.vim/autoload/plug.vim

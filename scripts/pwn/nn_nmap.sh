4d6c71dc2834b3e3145e5cd3bc7cc187d9481034
r4xjs <r4xjs@lowsec.net>
2019-11-24 21:20:54 +0100

    nmap sCV scan helper

--- scripts/pwn/nn_nmap.sh
+++ scripts/pwn/nn_nmap.sh
@@ -1,5 +1,9 @@
 #!bin/zsh
 
-nn_nmap_all_ports(){
+nn_nmap_tcp_all_ports(){
     print -z 'sudo nmap --nsock-engine epoll --defeat-rst-ratelimit -n -vvv -sS -T4 -oA all_tcp_ports -p- '
+}
+
+nn_nmap_tcp_sCV(){
+    print -z 'while read -r t; do host=${t%%:*};ports=${t##*: }; sudo nmap -sCV -p "$ports" -oA "${host}_sCV" -vvv "$host"  ; done <<(/home/user/.scr/pwn/gnmap_open_ports < **/*.gnmap'
 }

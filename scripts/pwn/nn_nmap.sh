e39b4abeff93b96f376bc31b78678ffceff739ec
r4xjs <r4xjs@lowsec.net>
2019-11-25 23:53:57 +0100

    ip list helper

--- scripts/pwn/nn_nmap.sh
+++ scripts/pwn/nn_nmap.sh
@@ -1,9 +1,13 @@
 #!bin/zsh
 
 nn_nmap_tcp_all_ports(){
-    print -z 'sudo nmap --nsock-engine epoll --defeat-rst-ratelimit -n -vvv -sS -T4 -oA all_tcp_ports -p- '
+    print -z 'sudo nmap --nsock-engine epoll --defeat-rst-ratelimit -n -vvv -sS -d -T4 -oA all_tcp_ports -p- '
 }
 
 nn_nmap_tcp_sCV(){
-    print -z 'while read -r t; do host=${t%%:*};ports=${t##*: }; sudo nmap -sCV -p "$ports" -oA "${host}_sCV" -vvv "$host"  ; done <<(/home/user/.scr/pwn/gnmap_open_ports < **/*.gnmap'
+    print -z 'while read -r t; do host=${t%%:*};ports=${t##*: }; sudo nmap -sCV -p "$ports" -oA "${host}_sCV" -vvv -d "$host"  ; done <<(/home/user/.scr/pwn/gnmap_open_ports < **/*.gnmap)'
+}
+
+nmap_ip_list(){
+    nmap -sL -n -iL "$1" | cut -d' ' -f5 | grep -P '^\d' --color=never 
 }

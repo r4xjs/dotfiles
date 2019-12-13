160f57f0f09cc45623f502af60035be06eb8d5be
r4xjs <raxjs@lowsec.net>
2019-12-13 11:59:19 +0100

    fix openssl self signed helper

--- scripts/nn_misc.sh
+++ scripts/nn_misc.sh
@@ -1,13 +1,13 @@
 #!/bin/zsh
 
 nn_self_signed_openssl(){
-    print -z 'openssl req -newkey rsa:4096 \
-            -x509 \
-            -sha256 \
-            -days 3650 \
-            -nodes \
-            -out example.crt \
-            -keyout example.key \
+    print -z 'openssl req -newkey rsa:4096 \\
+            -x509 \\
+            -sha256 \\
+            -days 3650 \\
+            -nodes \\
+            -out example.crt \\
+            -keyout example.key \\
             -subj "/C=SI/ST=Ljubljana/L=Ljubljana/O=Security/OU=IT Department/CN=www.example.com"'
 }
 

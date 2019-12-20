877702273b364c38de4a78049cd8064cc1fe52cb
r4xjs <raxjs@lowsec.net>
2019-12-20 18:27:13 +0100

    new hydra and gobuster helper

--- scripts/pwn/nn_brute.sh
+++ scripts/pwn/nn_brute.sh
@@ -2,5 +2,13 @@ nn_gobuster(){
     print -z 'gobuster dir -t 20 -w wordlist -o logfile -k -l -u baseurl'
 }
 
+nn_hydra_basicauth(){
+    print -z 'hydra -L userlist.txt -P passlist.txt -t 20 -s 443 -S -f -o out.log host https-get /path'
+}
+
+nn_hashcat_rules(){
+    print -z 'hashcat --stdout password.lst --rules /usr/share/hashcat/rules/best64.rule'
+}
+
 
 

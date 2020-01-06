3eb256433aa531350a7fc0930ecc78e29774fcd6
r4xjs <raxjs@lowsec.net>
2020-01-06 14:33:13 +0100

    definsive coding

--- scripts/fixes/fix_subject.sh
+++ scripts/fixes/fix_subject.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-email=$1
-subject=$(gpg -dq $email| grep Subject | head -1)
+email=`printf "%q" "$1"`
+subject=`printf "%q" $(gpg -dq $email| grep Subject | head -1)`
 temp=$(mktemp /tmp/email_XXXXXX)
-sed "s/Subject:.*/$subject/g" $email > $temp
-mv $temp "$email"
+sed "s/Subject:.*/$subject/g" "$email" > "$temp"
+mv "$temp" "$email"
 

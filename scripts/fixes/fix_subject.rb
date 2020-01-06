4c1a4580c9792d0ddc5ae813896e1a65a2cd773f
r4xjs <raxjs@lowsec.net>
2020-01-06 15:49:12 +0100

    fix_subject security

---
+++ scripts/fixes/fix_subject.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/ruby
+require 'shellwords'
+require 'tempfile'
+require 'fileutils'
+
+email=Shellwords.escape(ARGV[0])
+subject=`gpg -dq '#{email}' | grep "Subject:" | head -1`
+found = false
+Tempfile.open('temp_email_', '/tmp') do |tmp|
+    File.open(email, 'r').each do |line|
+        if(!found && line =~ /^Subject:/)
+            tmp.write("#{subject}")
+            found = true
+            next
+        end
+        tmp.write(line)
+    end
+    tmp.flush()
+    FileUtils.cp(tmp.path, email)
+end
--- scripts/fixes/fix_subject.sh
+++ scripts/fixes/fix_subject.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
-email=`printf "%q" "$1"`
-subject=`printf "%q" $(gpg -dq $email| grep Subject | head -1)`
+email="$1"
+subject="$(gpg -dq $email| grep Subject | head -1)"
+echo "$subject"
 temp=$(mktemp /tmp/email_XXXXXX)
 sed "s/Subject:.*/$subject/g" "$email" > "$temp"
 mv "$temp" "$email"

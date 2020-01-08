668a4d8745907aa953de06c9ad29f129cedaaab0
r4xjs <raxjs@lowsec.net>
2020-01-08 12:10:11 +0100

    fix subject: do nothing if not PEP email

--- scripts/fixes/fix_subject.rb
+++ scripts/fixes/fix_subject.rb
@@ -5,16 +5,18 @@ require 'fileutils'
 
 email=Shellwords.escape(ARGV[0])
 subject=`gpg -dq '#{email}' | grep "Subject:" | head -1`
-found = false
-Tempfile.open('temp_email_', '/tmp') do |tmp|
-    File.open(email, 'r').each do |line|
-        if(!found && line =~ /^Subject:/)
-            tmp.write("#{subject}")
-            found = true
-            next
-        end
-        tmp.write(line)
-    end
-    tmp.flush()
-    FileUtils.cp(tmp.path, email)
+if subject.length != 0
+  found = false
+  Tempfile.open('temp_email_', '/tmp') do |tmp|
+      File.open(email, 'r').each do |line|
+          if(!found && line =~ /^Subject:/)
+              tmp.write("#{subject}")
+              found = true
+              next
+          end
+          tmp.write(line)
+      end
+      tmp.flush()
+      FileUtils.cp(tmp.path, email)
+  end
 end

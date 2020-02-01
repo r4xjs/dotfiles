802e38934e1873e9c7f76710d77677cb0a007942
r4xjs <r4xjs@lowsec.net>
2020-02-01 19:44:05 +0100

    i3 wifi mode

---
+++ i3/wpa_cli.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+ssid=$(wpa_cli scan_result | rofi -dmenu | awk -F'\t' '{print $5}')
+idx=$(wpa_cli list_network | grep -P "\s+${ssid}\s+" | awk -F'\t' '{print $1}')
+if [ "$idx" -eq "$idx" ] 2> /dev/null; then
+    wpa_cli select_network "$idx"
+fi

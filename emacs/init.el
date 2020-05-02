5649c4b2db889caf930eebfc290440f6b598d814
r4xjs <r4xjs@lowsec.net>
2020-05-02 09:04:47 +0200

    customize cleanup

--- emacs/init.el
+++ emacs/init.el
@@ -1,4 +1,4 @@
-;;;;;;;;;; packages ;;;;;;;;;;
+g;;;;;;;;;; packages ;;;;;;;;;;
 
 (require 'package)
 (setq package-enable-at-startup nil)
@@ -107,6 +107,12 @@
 
 ;;;;;;;;;; settings ;;;;;;;;;;
 
+;; org-agenda-files
+(setq org-agenda-files '("~/todo.org"))
+
+;; enable disabled functions
+(put 'narrow-to-region 'disabled nil)
+(put 'dired-find-alternate-file 'disabled nil)
 
 ;; basic stuff
 (defalias 'yes-or-no-p 'y-or-n-p)
@@ -291,42 +297,8 @@
 (setenv "PAGER" "")
 
 
-;;;;;;;;;; do not edit ;;;;;;;;;;
+;;;;;;;;;; customize ;;;;;;;;;;
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-enabled-themes nil)
- '(custom-safe-themes
-   (quote
-    ("fa2b58bb98b62c3b8cf3b6f02f058ef7827a8e497125de0254f56e373abee088" "bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" default)))
- '(hl-todo-keyword-faces
-   (quote
-    (("TODO" . "#dc752f")
-     ("NEXT" . "#dc752f")
-     ("THEM" . "#2d9574")
-     ("PROG" . "#4f97d7")
-     ("OKAY" . "#4f97d7")
-     ("DONT" . "#f2241f")
-     ("FAIL" . "#f2241f")
-     ("DONE" . "#86dc2f")
-     ("NOTE" . "#b1951d")
-     ("KLUDGE" . "#b1951d")
-     ("HACK" . "#b1951d")
-     ("TEMP" . "#b1951d")
-     ("FIXME" . "#dc752f")
-     ("XXX+" . "#dc752f")
-     ("\\?\\?\\?+" . "#dc752f"))))
- '(org-agenda-files (quote ("~/todo.org")))
- '(pdf-view-midnight-colors (quote ("#b2b2b2" . "#292b2e")))
- '(send-mail-function (quote mailclient-send-it)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
-(put 'narrow-to-region 'disabled nil)
-(put 'dired-find-alternate-file 'disabled nil)
+;; keep my config clean
+(setq custom-file (concat user-emacs-directory "/custom.el"))
+

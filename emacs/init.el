8ad3a2804d4250888871a4338d6b89e800c2e2d0
r4xjs <github@lowsec.net>
2022-01-20 23:34:11 +0100

    evil agenda bindings

--- emacs/init.el
+++ emacs/init.el
@@ -86,6 +86,72 @@
   (evil-define-key 'insert ivy-minibuffer-map (kbd "C-k") 'ivy-previous-line)
   (evil-set-leader 'normal (kbd "SPC"))
   (evil-mode 1)
+
+  ;; src: https://gist.github.com/n2o4/65062bf378a6d6f575f71498deb20c80
+  (eval-after-load 'org-agenda
+ '(progn
+    (evil-set-initial-state 'org-agenda-mode 'normal)
+    (evil-define-key 'normal org-agenda-mode-map
+      (kbd "<RET>") 'org-agenda-switch-to
+      (kbd "\t") 'org-agenda-goto
+
+      "q" 'org-agenda-quit
+      "r" 'org-agenda-redo
+      "S" 'org-save-all-org-buffers
+      "gj" 'org-agenda-goto-date
+      "gJ" 'org-agenda-clock-goto
+      "gm" 'org-agenda-bulk-mark
+      "go" 'org-agenda-open-link
+      "s" 'org-agenda-schedule
+      "+" 'org-agenda-priority-up
+      "," 'org-agenda-priority
+      "-" 'org-agenda-priority-down
+      "y" 'org-agenda-todo-yesterday
+      "n" 'org-agenda-add-note
+      "t" 'org-agenda-todo
+      ":" 'org-agenda-set-tags
+      ";" 'org-timer-set-timer
+      "I" 'helm-org-task-file-headings
+      "i" 'org-agenda-clock-in-avy
+      "O" 'org-agenda-clock-out-avy
+      "u" 'org-agenda-bulk-unmark
+      "x" 'org-agenda-exit
+      "j"  'org-agenda-next-line
+      "k"  'org-agenda-previous-line
+      "vt" 'org-agenda-toggle-time-grid
+      "va" 'org-agenda-archives-mode
+      "vw" 'org-agenda-week-view
+      "vl" 'org-agenda-log-mode
+      "vd" 'org-agenda-day-view
+      "vc" 'org-agenda-show-clocking-issues
+      "g/" 'org-agenda-filter-by-tag
+      "o" 'delete-other-windows
+      "gh" 'org-agenda-holiday
+      "gv" 'org-agenda-view-mode-dispatch
+      "f" 'org-agenda-later
+      "b" 'org-agenda-earlier
+      "c" 'helm-org-capture-templates
+      "e" 'org-agenda-set-effort
+      "n" nil  ; evil-search-next
+      "{" 'org-agenda-manipulate-query-add-re
+      "}" 'org-agenda-manipulate-query-subtract-re
+      "A" 'org-agenda-toggle-archive-tag
+      "." 'org-agenda-goto-today
+      "0" 'evil-digit-argument-or-evil-beginning-of-line
+      "<" 'org-agenda-filter-by-category
+      ">" 'org-agenda-date-prompt
+      "F" 'org-agenda-follow-mode
+      "D" 'org-agenda-deadline
+      "J" 'org-agenda-next-date-line
+      "K" 'org-agenda-previous-date-line
+      "L" 'org-agenda-recenter
+      "P" 'org-agenda-show-priority
+      "R" 'org-agenda-clockreport-mode
+      "T" 'org-agenda-show-tags
+      "X" 'org-agenda-clock-cancel
+      "[" 'org-agenda-manipulate-query-add
+      "g\\" 'org-agenda-filter-by-tag-refine
+      "]" 'org-agenda-manipulate-query-subtract)))
   )
 ;; evil-overriding-maps and evil-intercept-maps variables.
 
@@ -319,6 +385,15 @@
   (setq org-agenda-files '(
 			 "/tmp/org/"
 			 ))
+
+  (setq org-agenda-custom-commands
+	'(
+	  ("D" "Demo block agenda"
+	   ((tags "test"
+		  ())))
+
+	  ))
+
   (setq org-todo-keywords '((sequence "TODO(t)" "NEXT(n)" "DONE(d)")))
   (setq org-return-follows-link t) ;; follow links with RET
 
